0401c27da4e278b4e3cc176dfdb89a604ab1a3ec
sigdelg <58709766+sigdelg@users.noreply.github.com>
2020-06-29 17:32:56 +0545

    Function Approach Enforced, other minor fixes

--- ProjectSrciptPreProcessing.py
+++ ProjectSrciptPreProcessing.py
@@ -67,6 +67,47 @@ def createDictionaryFromCSV(metadata,keyField,valueField):
                  gender.update({row[keyField]:row[valueField]})
     return gender  
 
+def preprocess(layer,metadataDir):
+    """
+    Performs preprocesing such as manipulating time fields, deleting unncessary field, avoiding repeatedly created fields on multiple execution.
+
+    Parameters
+    ----------
+    layer(Vector) :The layer to be preprocessed
+
+    Returns
+    -------
+    None.
+
+    """
+    #Remove Fields
+    RemoveFields(layer)
+    #Create dictionary for genders    
+    gender=createDictionaryFromCSV(metadataDir,'tag-id','animal-sex')
+    RemoveFields(layer,['Gender','trueDate','Hour','arbDate','arbHour'])
+    #add fields
+    caps=layer.dataProvider().capabilities()
+    if caps&QgsVectorDataProvider.AddAttributes:
+        layer.dataProvider().addAttributes([QgsField('Gender',QVariant.String)])
+        layer.dataProvider().addAttributes([QgsField('trueDate',QVariant.Date)])
+        layer.dataProvider().addAttributes([QgsField('Hour',QVariant.Int)])
+        layer.dataProvider().addAttributes([QgsField('arbDate',QVariant.String)])
+        layer.dataProvider().addAttributes([QgsField('arbHour',QVariant.Int)])
+    
+    #Read tag and incorporate.Shift the time by 19 hours, as we are using 19:00 hours as the start of origin
+    
+    with edit(layer):
+        for feature in layer.getFeatures():
+            feature.setAttribute(feature.fields().indexFromName('Gender'), gender.get(feature['tag_ident']))
+            regularTimeStamp=datetime.strptime(feature['timestamp'], '%Y-%m-%d %H:%M:%S')
+            adjustedTimeStamp=regularTimeStamp-timedelta(hours=19)
+            feature.setAttribute(layer.fields().indexFromName('arbDate'),adjustedTimeStamp.strftime('%Y-%m-%d'))
+            feature.setAttribute(layer.fields().indexFromName('trueDate'),regularTimeStamp.strftime('%Y-%m-%d'))
+            feature.setAttribute(layer.fields().indexFromName('Hour'),regularTimeStamp.hour)
+            feature.setAttribute(layer.fields().indexFromName('arbHour'),adjustedTimeStamp.hour)
+            layer.updateFeature(feature)
+    layer.updateFields()
+
 def getUniqueValues(layer,field):
     """  
 
@@ -95,55 +136,49 @@ def getUniqueValuesFromList(inlist):
             values.append(feature)
     return values
     
+def get_daily_data(layer,folder):
+    """
+    Prepares daily data from each bird and save them separate shapefiles, each of them containing data for that bird and that particular day.
+
+    Parameters
+    ----------
+    layer (string): The layer containing source data
+    folder (string):path to the folder containing all of the files. All shapefiles will be stored inside it.
+    Returns
+    -------
+    None.
+
+    """
+    #Get unique value for tags
+    if caps & QgsVectorDataProvider.ChangeAttributeValues:
+        tags=getUniqueValues(layer,'tag_ident')
+        for tag in tags:
+            selected=processing.run('qgis:selectbyattribute',{'INPUT':layer,'FIELD':'tag_ident','OPERATOR':0,'VALUE':tag,'METHOD':0})
+            selection = layer.selectedFeatures()
+            dates=list()
+            for features in selection:
+                dates.append(features['arbDate'])
+            #Get Unique Values for arbitrary Date
+            datelist=getUniqueValuesFromList(dates)
+            for mydate in datelist:
+                outputfilename=tag+"D"+mydate.replace("-","_")+".shp"
+                outputshp=os.path.join(folder,outputfilename)
+                expres=' \"tag_ident\" = \'{tag}\' AND \"arbDate\" = \'{date}\' '.format(tag=tag,date=mydate)
+                processing.run('qgis:selectbyexpression',{ 'EXPRESSION' : expres, 'INPUT' : layer, 'METHOD' : 0 })
+                processing.run('native:saveselectedfeatures',{'INPUT':layer,'OUTPUT':outputshp})
+                print("{output} file written to {folder}".format(output=outputfilename,folder=folder))
+#
+#
+#
+#
+#
 #Main Program
 #Add vector data   
 fileDir = "E:\\Masters\\Notes\\Sem2\\Python\\Project\\Data\\demopoints.shp"
 metadataDir="E:\\Masters\\Notes\\Sem2\\Python\\Project\\Data\\metadata.csv"
 layer = OpenFile(fileDir)
-#Remove Fields
-RemoveFields(layer)
-#Create dictionary for genders    
-gender=createDictionaryFromCSV(metadataDir,'tag-id','animal-sex')
-
-#Add fields
-#Remove existing fields first
-RemoveFields(layer,['Gender','trueDate','Hour','arbDate','arbHour'])
-#add fields
+#Load capabilities
 caps=layer.dataProvider().capabilities()
-if caps&QgsVectorDataProvider.AddAttributes:
-    layer.dataProvider().addAttributes([QgsField('Gender',QVariant.String)])
-    layer.dataProvider().addAttributes([QgsField('trueDate',QVariant.Date)])
-    layer.dataProvider().addAttributes([QgsField('Hour',QVariant.Int)])
-    layer.dataProvider().addAttributes([QgsField('arbDate',QVariant.String)])
-    layer.dataProvider().addAttributes([QgsField('arbHour',QVariant.Int)])
-
-#Read tag and incorporate.Shift the time by 19 hours, as we are using 19:00 hours as the start of origin
-
-with edit(layer):
-    for feature in layer.getFeatures():
-        feature.setAttribute(feature.fields().indexFromName('Gender'), gender.get(feature['tag_ident']))
-        regularTimeStamp=datetime.strptime(feature['timestamp'], '%Y-%m-%d %H:%M:%S')
-        adjustedTimeStamp=regularTimeStamp-timedelta(hours=19)
-        feature.setAttribute(layer.fields().indexFromName('arbDate'),adjustedTimeStamp.strftime('%Y-%m-%d'))
-        feature.setAttribute(layer.fields().indexFromName('trueDate'),regularTimeStamp.strftime('%Y-%m-%d'))
-        feature.setAttribute(layer.fields().indexFromName('Hour'),regularTimeStamp.hour)
-        feature.setAttribute(layer.fields().indexFromName('arbHour'),adjustedTimeStamp.hour)
-        layer.updateFeature(feature)
-layer.updateFields()
-
-#Get unique value for tags
-if caps & QgsVectorDataProvider.ChangeAttributeValues:
-    tags=getUniqueValues(layer,'tag_ident')
-    for tag in tags:
-        selected=processing.run('qgis:selectbyattribute',{'INPUT':layer,'FIELD':'tag_ident','OPERATOR':0,'VALUE':tag,'METHOD':0})
-        selection = layer.selectedFeatures()
-        dates=list()
-        for features in selection:
-            dates.append(features['arbDate'])
-        #Get Unique Values for arbitrary Date
-        datelist=getUniqueValuesFromList(dates)
-        for mydate in datelist:
-            print(tag,mydate)
-            expres=' \"tag_ident\" = \'{v01}\' AND \"arbDate\" = \'{v02}\' '.format(v01=tag,v02=mydate)
-            selecteds=processing.run('qgis:selectbyexpression',{ 'EXPRESSION' : expres, 'INPUT' : 'E:/Masters/Notes/Sem2/Python/Project/Data/demopoints.shp', 'METHOD' : 0 })
-            #saved=processing.run('native:saveselectedfeatures',{'INPUT':layer,'OUTPUT':'memory'})
+preprocess(layer,metadataDir)
+outputPath="E:\\Outputs"
+get_daily_data(layer,outputPath)
